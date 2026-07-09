d2f494d45148588bbf3cab50c08f72c11401491f
sweta <sweeta.gumanju@gmail.com>
2026-07-09 16:16:08 +0545

    2 string compressor

--- string_compressor/lib/string_compressor.rb
+++ string_compressor/lib/string_compressor.rb
@@ -1,3 +1,20 @@
-def string_compressor(string)
-  # Write your code here!
+def string_compressor(str)
+  return str if str.empty?
+
+  compressed = ""
+  count = 1
+
+  (1...str.length).each do |i|
+    if str[i] == str[i - 1]
+      count += 1
+    else
+      compressed += str[i - 1] + count.to_s
+      count = 1
+    end
+  end
+
+  compressed += str[-1] + count.to_s
+
+  # Return the shorter string
+  compressed.length < str.length ? compressed : str
 end
